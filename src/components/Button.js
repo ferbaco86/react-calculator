2b616e6f78c3ae0884c16e421c3c599210253e9a
Fernando Bahamondes <ferbaco@gmail.com>
2021-01-21 13:47:47 -0300

    Update button

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,13 +2,16 @@ import PropTypes from 'prop-types';
 
 const Button = props => {
   const { name } = props;
+  const handleClick = buttonName => {
+    props.clickHandler(buttonName);
+  };
   return (
-    <button type="button">
+    <button type="button" onClick={() => handleClick}>
       {name}
     </button>
   );
 };
 
-Button.propTypes = { name: PropTypes.string.isRequired };
+Button.propTypes = { name: PropTypes.string.isRequired, clickHandler: PropTypes.func.isRequired };
 
 export default Button;
