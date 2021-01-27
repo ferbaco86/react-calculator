c5df7a1f2c6772203d20daa26ea93fcd8d41739f
Fernando Bahamondes <ferbaco@gmail.com>
2021-01-26 21:34:48 -0300

    Add quotes

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,7 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 import NavBar from './NavBar';
 
-const Quotes = () => (
-  <NavBar />
-);
+const QuoteTitle = styled.h1`
+font-size: 2.6rem;
+text-align: center;
+padding: 1.5rem;
+margin-top: 10rem;
+color: white;`;
+
+const Quotes = () => {
+  const randomNumber = Math.round(Math.random() * 20);
+  const [quote, setQuote] = useState('');
+  const getQuotes = () => {
+    fetch('quotes.json', {
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    }).then(response => response.json()).then(myJson => {
+      setQuote(myJson);
+    });
+  };
+  useEffect(() => {
+    getQuotes();
+  }, []);
+  return (
+    <>
+      <NavBar />
+      <QuoteTitle>{quote[randomNumber]}</QuoteTitle>
+    </>
+  );
+};
 
 export default Quotes;
